7e6c55f0f0c6f2fc10db3244b20ee97496699344
Rosemary <dayaftertomorrow.davies@gmail.com>
2017-02-13 09:23:06 -0800

    Update Cars.java

--- Cars.java
+++ Cars.java
@@ -1,40 +1,88 @@
-/*
-* Project Description
-*/
+/**
+ * Class {@Cars} makes car objects
+ * @author  Carl Wheat
+ * @author Rosemary Davies
+ * @since 8.01
+ * Computer Science Foundations, Spring
+ */
+
 
 import java.util.*;
 
 public class Car {
 
-// Public or privtte?
-private int position;
-private int gas;
-private int distance;
+ // Part 1:
 
-// Constructor Car
+    // List of our fields
 
-public class Car (int position, int gas, int distance){
-  this.position = position;
-  this.gas = gas;
-  this.distance = distance;
+    private int position;
+    private int gas;
+    private int distance;
+
+    // Default Constructor
+
+    public Cars (int position, int gas, int distance){
+        this.position = position;
+        this.gas = gas;
+        this.distance = distance;
+    }
+
+
+    // Method to print car objects
+    public String toString() {
+        String myStr = "";
+        myStr = "position: %d \n";, this.position;
+        myStr.concat("gas: %d \n";, this.gas);
+        myStr.concat("distance: %d \n";, this.distance);
+        return myStr;
+
+    }
+
+    // Getters:
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getGas() {
+        return gas;
+    }
+
+
+    // Part 2:
+
+    // Recursive function
+
+    public static int findTheWinnter(Cars cars[], int size){
+
+    }
+
+    // Method for car information:
+
+    // Creates array about every car on the road, returns number of cars on road
+    // -1 is sentinel
+    public static int fillCarArray(Cars cars[], int maxsize){
+
+    }
+
+    // Prints information about every car
+    public static void printCarArray(Cars cars[], int size){
+
+    }
+
+    // Prints log of travel for the winning car
+    public static void log(Cars cars[], int size, int start){
+
+    }
+
+    // Returns next car on the road given current position
+    public static Cars nextCar(Cars cars[], int currentPosition, int size){
+
+    }
+
 }
 
-public String toString() {
-// 
-}
-
-// Getters:
-
-public int getPosition() {
-  return this.position;
-}
-
-public int getDistance() {
-  return this.distance;
-}
-
-public int getGas() {
-  return this.gas;  
-}
-
-}
