15c4ef68f1134eaa992c3fccdfed36ce42cad895
irvingwheat <irvingwheat4@gmail.com>
2017-02-13 18:36:33 -0800

    Update and rename Cars.java to Car.java

--- Car.java
+++ Car.java
@@ -1,39 +1,32 @@
-/**
- * Class {@Cars} makes car objects
- * @author  Carl Wheat
- * @author Rosemary Davies
- * @since 8.01
- * Computer Science Foundations, Spring
- */
-
-
-import java.util.*;
-
 public class Car {
 
  // Part 1:
 
     // List of our fields
 
-    private int position;
-    private int gas;
-    private int distance;
+    public int position;
+    public int gas;
+    public int distance;
 
     // Default Constructor
 
-    public Cars (int position, int gas, int distance){
+    public Car(int position, int gas, int distance){
         this.position = position;
         this.gas = gas;
         this.distance = distance;
     }
-
+    public Car(){
+        this.position = 0;
+        this.gas = 0;
+        this.distance = 0;
+    }
 
     // Method to print car objects
     public String toString() {
         String myStr = "";
-        myStr = "position: %d \n";, this.position;
-        myStr.concat("gas: %d \n";, this.gas);
-        myStr.concat("distance: %d \n";, this.distance);
+        myStr = "position: \n" + this.position;
+        myStr.concat("gas: \n" + this.gas);
+        myStr.concat("distance: \n" + this.distance);
         return myStr;
 
     }
@@ -57,7 +50,7 @@ public class Car {
 
     // Recursive function
 
-    public static int findTheWinnter(Cars cars[], int size){
+    public static void findTheWinnter(Car cars[], int size){
 
     }
 
@@ -65,24 +58,23 @@ public class Car {
 
     // Creates array about every car on the road, returns number of cars on road
     // -1 is sentinel
-    public static int fillCarArray(Cars cars[], int maxsize){
+    public static void fillCarArray(Car cars[], int maxsize){
 
     }
 
     // Prints information about every car
-    public static void printCarArray(Cars cars[], int size){
+    public static void printCarArray(Car cars[], int size){
 
     }
 
     // Prints log of travel for the winning car
-    public static void log(Cars cars[], int size, int start){
+    public static void log(Car cars[], int size, int start){
 
     }
 
     // Returns next car on the road given current position
-    public static Cars nextCar(Cars cars[], int currentPosition, int size){
+    public static void nextCar(Car cars[], int currentPosition, int size){
 
     }
 
 }
-
