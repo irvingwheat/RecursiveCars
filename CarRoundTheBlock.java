e83cb8229130068c0cf839c1f1492fb361a8e8cb
irvingwheat <irvingwheat4@gmail.com>
2017-02-13 18:38:20 -0800

    Update CarRoundTheBlock.java

--- CarRoundTheBlock.java
+++ CarRoundTheBlock.java
@@ -1,32 +1,42 @@
-import java.util.scanner;
+import java.util.Scanner;
 public class CarRoundTheBlock {
-	public static int findTheWinner (Car cars[], int size) {
-	
-	}
-	public static int fillCarArray (Car cars[], int maxsize) {
-		int maxsize = 100;
-		Scanner input = new Scanner(System.in);
-		int i = 0;
-		for (i = 0; i <= 100; ++i) {
-			int newInput = input.nextInt();
-			int newInput2 = input.nextInt();
-			if (newInput == -1 || newInput2 == -1) {
-				int numCars1 = cars.length;
-				return numCars1;
-			}
-			Car cars[i] = new Car(i, newInput, newInput2);
-			
-		}
-		int numCars = cars.length;
-		return numCars;
-	}
-	public static void printCarArray (Car cars[], int size) {
-		
-	}
-	public static void log (Car cars[], int size, int start) {
-		
-	}
-	public static Car nextCar (Car cars[], int currentPosition, int size) {
-	
-	}
+/*public static void findTheWinner (Car cars[], int size) {
+ 
+}*/
+  /*
+  public static void fillCarArray() {
+    
+  }
+  */
+  
+  public static String fillCarArray (Car cars[], int size) {
+
+     Scanner input = new Scanner(System.in);
+     
+     for (int i = 0; i < size; ++i) {
+        cars[i] = new Car();
+        cars[i].gas = input.nextInt();
+        cars[i].distance = input.nextInt();
+        
+        if (cars[i].gas == -1 || cars[i].distance == -1) {
+           int numCars1 = cars.length;
+           return "Number of Cars on the road: " + numCars1;
+        }
+        //cars[maxsize] = new Car(i, newInput, newInput2);
+     }
+     int numCars1 = cars.length;
+     return "Number of Cars on the road: " + numCars1;
+  }
+
+ public static String printCarArray(){ //(Car cars[], int size) {
+  return "I am a string";
+ }
+ 
+ /*
+ public static void log (Car cars[], int size, int start) {
+  
+ }
+ public static Car nextCar (Car cars[], int currentPosition, int size) {
+ 
+ } */
 }
